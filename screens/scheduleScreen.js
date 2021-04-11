0fc82cdb475898b0c19ddd9dc64024b3fad5855c
Manan861 <62825867+Manan861@users.noreply.github.com>
2021-04-11 12:59:26 +0530

    Add files via upload

---
+++ screens/scheduleScreen.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  Modal,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+  ScrollView
+} from "react-native";
+import Spreadsheet from "react-spreadsheet";
+const RangeView = ({ cell }) => (
+  <input
+    type="range"
+    value={cell.value}
+    disabled
+    style={{ pointerEvents: "none" }}
+  />
+);
+
+const RangeEdit = ({ cell, onChange }) => (
+  <input
+    type="range"
+    onChange={(e) => {
+      onChange({ ...cell, value: e.target.value });
+    }}
+    value={cell.value || 0}
+    autoFocus
+  />
+);
+
+
+const data = [
+  [{ value: "Time/Day" }, { value: "Monday" }, {value:"Tuesday"}, {value:"Wednesday"}, {value:"Thursday"}, {value:"Friday"}],
+  [{ value: "9:00-9:45" },{ value: 100,  DataEditor: RangeEdit }],
+  [{value:  "9.45-10:30"}],
+  [{value:  "10:45-11:15"}],
+  [{value:  "11:15-12:00"}],
+  [{value:  "12:00-12:45"}],
+  [{value:  "12:45-1:30"}],
+  [{value: "2:15-3:00"}],
+];
+
+const MyComponent = () => <Spreadsheet data={data} />;
+export default class scheduleScreen extends Component{
+  constructor(){
+    super()
+    this.state={
+      className:''
+    }
+  }
+  render(){
+    return(
+     
+      <View>
+        <Text>
+          
+          Schedule Screen 
+        </Text>
+        <MyComponent></MyComponent>
+      </View>
+    )
+  }
+}
